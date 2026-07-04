ca122b71cf16c317fe3f07326f8290ce9f02ecd7
Dmitry Sitnikov <betani.elvin@mail.ru>
2026-07-04 21:10:53 +0300

    Fix feature tasks

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 
+	core_config "github.com/Sinhofazatron/tasks-go/internal/core/config"
 	core_logger "github.com/Sinhofazatron/tasks-go/internal/core/logger"
 	core_pgx_pool "github.com/Sinhofazatron/tasks-go/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/Sinhofazatron/tasks-go/internal/core/transport/http/middleware"
@@ -21,12 +22,9 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	timeZone = time.UTC
-)
-
 func main() {
-	time.Local = timeZone
+	cfg := core_config.NewConfigMust()
+	time.Local = cfg.TimeZone
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -41,7 +39,7 @@ func main() {
 	}
 	defer logger.Close()
 
-	logger.Debug("application time zone", zap.Any("zone", timeZone))
+	logger.Debug("application time zone", zap.Any("zone", time.Local))
 
 	logger.Debug("initializing postgres connection pool")
 	pool, err := core_pgx_pool.NewPool(ctx, core_pgx_pool.NewConfigMust())
